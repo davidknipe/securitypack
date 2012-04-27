054c5ccff846946297d4c8c609611dae13fe2a7c
davidknipe_cp <SND\davidknipe_cp>
2012-04-27 11:27:47 +0000

    v1.1 missing files

---
+++ trunk/PageTypeSecurityAddOn/ScheduledJobs/SetupPageTypeSecurity.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EPiServer.PlugIn;
+using EPiServer.Security;
+using EPiServer.BaseLibrary.Scheduling;
+
+namespace SecurityPack.PageTypeSecurityAddOn.ScheduledJobs
+{
+    [ScheduledPlugIn(DisplayName = "SecurityPack : Page type security set up", Description = "Adds Change access to all page types that currently have Read access", SortIndex = 9000)]
+    public class SetupPageTypeSecurity : JobBase
+    {
+        private bool stopped = false;
+
+        public SetupPageTypeSecurity()
+            : base()
+        {
+            this.IsStoppable = true;
+        }
+
+        public override void Stop()
+        {
+            stopped = true;
+            base.Stop();
+        }
+
+        public override string Execute()
+        {
+            try
+            {
+                int updatedCount = 0;
+
+                //Iterate all page types
+                foreach (var pt in EPiServer.DataAbstraction.PageType.List())
+                {
+                    //Set Change rights where Read rights already exist
+                    bool changed = false;
+                    AccessControlList update = pt.ACL.Copy();
+                    foreach (var key in pt.ACL)
+                    {
+                        if (key.Value.Access.HasFlag(AccessLevel.Create) && !key.Value.Access.HasFlag(AccessLevel.Edit) && key.Value.Access.HasFlag(AccessLevel.Publish))
+                        {
+                            update[key.Key] = new AccessControlEntry(key.Key, AccessLevel.Create | AccessLevel.Edit | AccessLevel.Publish);
+                            changed = true;
+                        }
+                    }
+                    if (changed)
+                    {
+                        pt.ACL = update;
+                        pt.Save();
+                        this.OnStatusChanged("Applying updates for page type: '" + pt.Name + "'");
+                        updatedCount++;
+                    }
+                    if (this.stopped)
+                        break;
+                }
+                if (updatedCount > 0)
+                {
+                    return updatedCount.ToString() + " page types were updated";
+                }
+                else
+                {
+                    return "No page types were updated";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "EXCEPTION executing job: " + ex.ToString();
+            }
+        }
+    }
+}
