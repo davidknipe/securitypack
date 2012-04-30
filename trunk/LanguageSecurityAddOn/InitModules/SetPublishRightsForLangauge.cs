57e4e59c2a1445e1665b9e3f5a5d0222a5782bc0
davidknipe_cp <davidknipe@users.noreply.github.com>
2012-04-30 13:32:12 +0000

    Adding LangaugeSecurityAddOn v1.0

---
+++ trunk/LanguageSecurityAddOn/InitModules/SetPublishRightsForLangauge.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Reflection;
+using System.Web;
+using EPiServer.Framework;
+using EPiServer.Security;
+using EPiServer.UI;
+using EPiServer.UI.Admin;
+using EPiServer.UI.WebControls;
+using System.Web.UI;
+using EPiServer.UI.Edit;
+using EPiServer.DataAbstraction;
+
+namespace SecurityPack.LanguageSecurityAddOn.InitModules
+{
+    [InitializableModule]
+    [ModuleDependency((typeof(EPiServer.Web.InitializationModule)))]
+    public class SetPublishRightsForLangauge : IInitializableHttpModule
+    {
+        public void InitializeHttpEvents(System.Web.HttpApplication application)
+        {
+            application.PreRequestHandlerExecute += context_PreRequestHandlerExecute;
+        }
+
+        private void context_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            //Get the Request handler from the Context
+            HttpContext context = ((HttpApplication)sender).Context;
+
+            //Only try this if we have a handler
+            if (context.Handler != null && context.User != null && context.User.Identity != null && context.User.Identity.IsAuthenticated)
+            {
+                //Check if the request handler is of the right type
+                if (context.Handler.GetType().IsSubclassOf(typeof(EPiServer.UI.Edit.EditPanel)))
+                {
+                    SystemPageBase pageBase = (EditPanel)context.Handler;
+                    pageBase.PreRender += pageBase_PreRender;
+                }
+            }
+        }
+
+        void pageBase_PreRender(object sender, EventArgs e)
+        {
+            EPiServer.UI.Edit.EditPanel editPanel = (EPiServer.UI.Edit.EditPanel)sender;
+
+            EditPageButtonControl editPageButtonControl = FindControl<EditPageButtonControl>(editPanel);
+
+            if (editPageButtonControl != null && editPageButtonControl.Page != null)
+            {
+                //Check if the user has Publish access to the page language
+                LanguageBranch pageLang = LanguageBranch.Load(editPanel.CurrentPage.LanguageID);
+                if (pageLang.ACL.QueryDistinctAccess(PrincipalInfo.CurrentPrincipal, AccessLevel.Publish) == false)
+                {
+                    // The user does not have Publish access to the lanaguage so remove any publish 
+                    // rights, this is temporary since the ACL is not saved with the page
+                    AccessControlList newList = editPanel.CurrentPage.ACL.Copy();
+                    foreach (var key in editPanel.CurrentPage.ACL)
+                    {
+                        if (key.Value.Access.HasFlag(AccessLevel.Publish))
+                        {
+                            AccessLevel level = key.Value.Access;
+                            level = level ^ AccessLevel.Publish;
+                            newList[key.Key] = new AccessControlEntry(key.Key, level);
+                        }
+                    }
+                    editPanel.CurrentPage.ACL = newList;
+                }
+            }
+        }
+
+        public void Initialize(EPiServer.Framework.Initialization.InitializationEngine context) { }
+
+        public void Preload(string[] parameters) { }
+
+        public void Uninitialize(EPiServer.Framework.Initialization.InitializationEngine context) { }
+
+        // try to locate control of type T with ID==id
+        // recurses into each childcontrol until first match is found
+        protected T FindControl<T>(Control control, string id) where T : Control
+        {
+            try
+            {
+                T controlTest = control as T;
+
+                if (null != controlTest && (null == id || controlTest.ID.Equals(id)))
+                    return controlTest;
+
+                foreach (Control c in control.Controls)
+                {
+                    controlTest = FindControl<T>(c, id);
+                    if (null != controlTest)
+                        return controlTest;
+                }
+
+                return null;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        protected T FindControl<T>(Control control) where T : Control
+        {
+            return FindControl<T>(control, null);
+        }
+    }
+}
---
+++ trunk/LanguageSecurityAddOn/ScheduledJobs/SetupLanguageSecurity.cs
@@ -0,0 +1,73 @@
+﻿using EPiServer.BaseLibrary.Scheduling;
+using EPiServer.PlugIn;
+using EPiServer.Security;
+using System;
+using System.Collections.Generic;
+
+namespace SecurityPack.LanguageSecurityAddOn.ScheduledJobs
+{
+    [ScheduledPlugIn(
+        DisplayName = "SecurityPack : Language security set up", 
+        Description = "Adds Publish access to all languages that currently have Change access <hr/><strong>WARNING!</strong> This job may affect existing langauge security settings. It is intended to be used as a one off when first installing SecurityPack.LanguageSecurityAddon<hr/>", 
+        SortIndex = 9001)]
+    public class SetupLanguageSecurity : JobBase
+    {
+        private bool stopped = false;
+
+        public SetupLanguageSecurity()
+            : base()
+        {
+            this.IsStoppable = true;
+        }
+
+        public override void Stop()
+        {
+            stopped = true;
+            base.Stop();
+        }
+
+        public override string Execute()
+        {
+            try
+            {
+                List<string> updatedList = new List<string>();
+
+                //Iterate all languages
+                foreach (var lang in EPiServer.DataAbstraction.LanguageBranch.ListAll())
+                {
+                    //Add Publish rights where Read rights already exist
+                    bool changed = false;
+                    AccessControlList update = lang.ACL.Copy();
+                    foreach (var key in lang.ACL)
+                    {
+                        if (key.Value.Access.HasFlag(AccessLevel.Edit) && !key.Value.Access.HasFlag(AccessLevel.Publish))
+                        {
+                            update[key.Key] = new AccessControlEntry(key.Key, AccessLevel.Edit | AccessLevel.Publish);
+                            changed = true;
+                        }
+                    }
+                    if (changed)
+                    {
+                        lang.ACL = update;
+                        lang.Save();
+                        this.OnStatusChanged("Applying updates for language: '" + lang.Name + "'");
+                        updatedList.Add(lang.LanguageID);
+                    }
+                    if (this.stopped)
+                        break;
+                }
+                if (updatedList.Count > 0)
+                {
+                    return "The following language branch(es) were updated: " + String.Join(", ", updatedList);
+                }
+                else
+                {
+                    return "No languages were updated";
+                }
+            }
+            catch (Exception ex)
+            {
+                return "EXCEPTION executing job: " + ex.ToString();
+            }
+        }
+    }}
